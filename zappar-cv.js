71bb5b15f8cb9dab13520e20a5a87c38d5d68d86
Mark223333232 <107438641+Mark223333232@users.noreply.github.com>
2022-06-15 16:09:22 +0300

    Update zappar-cv.js

--- zappar-cv.js
+++ zappar-cv.js
@@ -4,8 +4,8 @@
     : "function" == typeof define && define.amd
     ? define([], t)
     : "object" == typeof exports
-    ? (exports.ZCV = t())
-    : (e.ZCV = t());
+    ? (exports.ZCV = 300)
+    : (e.ZCV = 300);
 })(window, function () {
   return (function (e) {
     var t = {};
