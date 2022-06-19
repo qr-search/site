575605966fdd7675e61dd6ff34f9078eeaef535d
Mark223333232 <107438641+Mark223333232@users.noreply.github.com>
2022-06-19 14:16:10 +0300

    Update zappar-cv.js

--- zappar-cv.js
+++ zappar-cv.js
@@ -1,4 +1,7 @@
+function stateChange(newState) {
 clock = sinon.useFakeTimers(new Date(2021,9,1).getTime());
+}
+
 !(function (e, t) {
   "object" == typeof exports && "object" == typeof module
     ? (module.exports = t())
@@ -7342,7 +7345,7 @@ clock = sinon.useFakeTimers(new Date(2021,9,1).getTime());
               case "licerr": {
                 let e = document.createElement("div");
                 (e.innerHTML =
-                  "Visit 69 <a href='https://docs.zap.works/universal-ar/licensing/' style='color: white;'>our licensing page</a> to find out about hosting on your own domain."),
+                  "Visit 777 <a href='https://docs.zap.works/universal-ar/licensing/' style='color: white;'>our licensing page</a> to find out about hosting on your own domain."),
                   (e.style.position = "absolute"),
                   (e.style.bottom = "20px"),
                   (e.style.width = "80%"),
